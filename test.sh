2b93ad8388e0238577aba12a0eecde181abbacfc
Alexey Busygin <yaabusygin@gmail.com>
2022-09-25 16:21:05 +0300

    Parametrize test platform

---
+++ test.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+platforms="geerlingguy/docker-ubuntu2004-ansible:latest"
+
+for image in ${platforms}; do
+    echo "Running Molecule tests for '${image}'..."
+    TEST_IMAGE="${image}" molecule test --all
+    if [ $? -ne 0 ]; then
+        echo "Error: Molecule test failed" >&2
+        exit 1
+    fi
+done
+
+echo "Success: all tests are passed"
